0b368512a900a20016ab592e85a61842e0560d65
nosqldb <presidentccj@gmail.com>
2015-01-17 23:40:14 +0800

    update config

---
+++ config.default.js
@@ -0,0 +1,18 @@
+﻿var config = {
+  cookieSecret: 'nblog', 
+  db: 'nblog', 
+  host: 'localhost',
+  port:8890,
+  url:'mongodb://localhost/nblog',
+  name:"站点名称",
+    // 版块
+  tabs: [
+    ['essay', '随笔'],
+    ['review', '点评'],
+    ['travel','旅行'],
+    ['others', '其他']
+  ],
+};
+
+module.exports = config;
+module.exports.config = config;
